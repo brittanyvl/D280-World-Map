9d2bdc06b4c06aeceece7021653db482aa12c5b2
Brittany Campos <bmagnu1@wgu.edu>
2024-05-14 22:07:07 -0500

    Created event function on component ts file

--- D280project/src/app/api.service.ts
+++ D280project/src/app/api.service.ts
@@ -7,4 +7,10 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
+
+  fetchCountryData(countryID: string) {
+    let api = `https://api.worldbank.org/v2/country/${countryID}?format=json`
+    return this.http.get(api)
+  }
+
 }
